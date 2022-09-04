54258f5b45074def514f621bdd9e51f7f6511de0
Kenta Kariya <kentakariya@outlook.com>
2022-09-04 21:29:25 +0200

    chore(cli): add cli hints

--- src/main.rs
+++ src/main.rs
@@ -4,15 +4,18 @@ use anyhow::Ok;
 use clap::Parser;
 use url::Url;
 
+/// Generate a signed CloudFront URL using the private key from stdin.
 #[derive(Debug, Parser)]
 #[clap(author, version, about)]
 struct Args {
     #[clap(value_parser = Url::parse)]
     url: Url,
 
+    /// Validity in minutes
     #[clap(short, long, default_value_t = 60)]
     duration: u64,
 
+    /// CloudFront key ID
     #[clap(short, long)]
     key_id: String,
 }
