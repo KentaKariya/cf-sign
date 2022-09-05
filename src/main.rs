7f5fa2c4ec7b295c43bf5a2922ab1668f07b0da8
Kenta Kariya <kentakariya@outlook.com>
2022-09-05 21:00:22 +0200

    feat(cli): read options from config file

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,10 @@ use std::io::Read;
 
 use anyhow::Ok;
 use clap::Parser;
-use url::Url;
 use chrono::{Utc, Duration};
+use url::Url;
 
+mod options;
 mod sign;
 
 #[cfg(test)]
@@ -13,27 +14,29 @@ mod tests;
 /// Generate a signed CloudFront URL using the private key from stdin.
 #[derive(Debug, Parser)]
 #[clap(author, version, about)]
-struct Args {
+pub struct Args {
     #[clap(value_parser = Url::parse)]
     url: Url,
 
     /// Validity in seconds
-    #[clap(short, long, default_value_t = 3600)]
-    duration: u64,
+    #[clap(short, long)]
+    duration: Option<u64>,
 
     /// CloudFront key ID
     #[clap(short, long)]
-    key_id: String,
+    key_id: Option<String>,
 }
 
 fn main() -> anyhow::Result<()> {
     let args = Args::parse();
-    let expire_at = Utc::now() + Duration::seconds(args.duration as i64);
+    let config: _ = options::parse_options(&args)?;
+
+    let expire_at = Utc::now() + Duration::seconds(config.sign.duration as i64);
+
     let mut key = String::new();
     std::io::stdin().read_to_string(&mut key)?;
 
-
-    let signed_url = sign::sign(args.url.as_ref(), expire_at, &args.key_id, &key)?;
+    let signed_url = sign::sign(args.url.as_ref(), expire_at, &config.sign.key_id, &key)?;
     println!("{}", signed_url);
 
     Ok(())
---
+++ src/options.rs
@@ -0,0 +1,41 @@
+use std::path::PathBuf;
+
+use anyhow::Context;
+use config::{FileSourceFile, FileFormat};
+use serde::Deserialize;
+
+use crate::Args;
+
+const QUALIFIER: &'static str = "com";
+const ORGANIZATION: &'static str = "kentakariya";
+const APPLICATION: &'static str = "cf-sign";
+
+#[derive(Debug, Deserialize)]
+pub struct Config {
+    pub sign: Sign,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct Sign {
+    pub duration: u64,
+    pub key_id: String,
+}
+
+pub fn parse_options(overrides: &Args) -> anyhow::Result<Config> {
+    let config_file: config::File<FileSourceFile, FileFormat> = get_config_file()?.into();
+
+    config::Config::builder()
+        .add_source(config_file)
+        .set_override_option("sign.duration", overrides.duration)?
+        .set_override_option("sign.key_id", overrides.key_id.clone())?
+        .build()?
+        .try_deserialize::<Config>()
+        .context("Could not parse config file")
+}
+
+fn get_config_file() -> anyhow::Result<PathBuf> {
+    directories::ProjectDirs::from(QUALIFIER, ORGANIZATION, APPLICATION)
+        .map(|d| d.config_dir().join("config.toml"))
+        .context("Could not determine location for config file")
+}
+
